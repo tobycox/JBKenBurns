f33b8876bc146761279006257ecb916f88fa234c
Toby Cox <toby@carnivallabs.com>
2014-06-03 20:08:32 +1200

    Simplifying some of the scale and pan code

--- KenBurns/JBKenBurnsView.m
+++ KenBurns/JBKenBurnsView.m
@@ -129,136 +129,50 @@ enum JBSourceMode {
     UIImage *image = self.currentImage;
     UIImageView *imageView = nil;
     
-    float resizeRatio   = -1;
-    float widthDiff     = -1;
-    float heightDiff    = -1;
-    float originX       = -1;
-    float originY       = -1;
-    float zoomInX       = -1;
-    float zoomInY       = -1;
-    float moveX         = -1;
-    float moveY         = -1;
-    float frameWidth    = _isLandscape ? self.bounds.size.width: self.bounds.size.height;
-    float frameHeight   = _isLandscape ? self.bounds.size.height: self.bounds.size.width;
+    float scaleFactor = 1.4;
+    float width = self.bounds.size.width;
+    float height = self.bounds.size.height;
+    float marginX = ((width * scaleFactor) - width) / 2;
+    float marginY = ((height * scaleFactor) - height) / 2;
+    float originX = -1;
+    float originY = -1;
     
-    // Wider than screen 
-    if (image.size.width > frameWidth)
-    {
-        widthDiff  = image.size.width - frameWidth;
-        
-        // Higher than screen
-        if (image.size.height > frameHeight)
-        {
-            heightDiff = image.size.height - frameHeight;
-            
-            if (widthDiff > heightDiff) 
-                resizeRatio = frameHeight / image.size.height;
-            else
-                resizeRatio = frameWidth / image.size.width;
-            
-        // No higher than screen [OK]
-        }
-        else
-        {
-            heightDiff = frameHeight - image.size.height;
-            
-            if (widthDiff > heightDiff) 
-                resizeRatio = frameWidth / image.size.width;
-            else
-                resizeRatio = self.bounds.size.height / image.size.height;
-        }
-        
-    // No wider than screen
-    }
-    else
-    {
-        widthDiff  = frameWidth - image.size.width;
-        
-        // Higher than screen [OK]
-        if (image.size.height > frameHeight)
-        {
-            heightDiff = image.size.height - frameHeight;
-            
-            if (widthDiff > heightDiff) 
-                resizeRatio = image.size.height / frameHeight;
-            else
-                resizeRatio = frameWidth / image.size.width;
-            
-        // No higher than screen [OK]
-        }
-        else
-        {
-            heightDiff = frameHeight - image.size.height;
-            
-            if (widthDiff > heightDiff) 
-                resizeRatio = frameWidth / image.size.width;
-            else
-                resizeRatio = frameHeight / image.size.height;
-        }
-    }
-    
-    // Resize the image.
-    float optimusWidth  = (image.size.width * resizeRatio) * enlargeRatio;
-    float optimusHeight = (image.size.height * resizeRatio) * enlargeRatio;
-    imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, optimusWidth, optimusHeight)];
+    imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, width, height)];
+    [imageView setContentMode:UIViewContentModeScaleAspectFill];
     imageView.backgroundColor = [UIColor blackColor];
-    
-    // Calcule the maximum move allowed.
-    float maxMoveX = optimusWidth - frameWidth;
-    float maxMoveY = optimusHeight - frameHeight;
     
     float rotation = (arc4random() % 9) / 100;
     
     switch (arc4random() % 4) {
-        case 0:
+        case 0: // Top left
             originX = 0;
             originY = 0;
-            zoomInX = 1.25;
-            zoomInY = 1.25;
-            moveX   = -maxMoveX;
-            moveY   = -maxMoveY;
             break;
             
-        case 1:
-            originX = 0;
-            originY = frameHeight - optimusHeight;
-            zoomInX = 1.10;
-            zoomInY = 1.10;
-            moveX   = -maxMoveX;
-            moveY   = maxMoveY;
-            break;
-            
-        case 2:
-            originX = frameWidth - optimusWidth;
+        case 1: // Top right
+            originX = -marginX;
             originY = 0;
-            zoomInX = 1.30;
-            zoomInY = 1.30;
-            moveX   = maxMoveX;
-            moveY   = -maxMoveY;
+            break;
+            
+        case 2: // Bottom left
+            originX = 0;
+            originY = -marginY;
             break;
             
         case 3:
-            originX = frameWidth - optimusWidth;
-            originY = frameHeight - optimusHeight;
-            zoomInX = 1.20;
-            zoomInY = 1.20;
-            moveX   = maxMoveX;
-            moveY   = maxMoveY;
+            originX = -marginX;
+            originY = -marginY;
             break;
             
         default:
-            NSLog(@"Unknown random number found in JBKenBurnsView _animate");
+            [NSException raise:@"Unknown random number found in JBKenBurnsView _animate" format:@""];
             break;
     }
-    
-//    NSLog(@"W: IW:%f OW:%f FW:%f MX:%f",image.size.width, optimusWidth, frameWidth, maxMoveX);
-//    NSLog(@"H: IH:%f OH:%f FH:%f MY:%f\n",image.size.height, optimusHeight, frameHeight, maxMoveY);
-    
     CALayer *picLayer    = [CALayer layer];
     picLayer.contents    = (id)image.CGImage;
-    picLayer.anchorPoint = CGPointMake(0, 0); 
-    picLayer.bounds      = CGRectMake(0, 0, optimusWidth, optimusHeight);
-    picLayer.position    = CGPointMake(originX, originY);
+    picLayer.anchorPoint = CGPointMake(0, 0);
+    picLayer.bounds      = CGRectMake(0, 0, width, height);
+    picLayer.position    = CGPointMake(0, 0);
     
     [imageView.layer addSublayer:picLayer];
     
@@ -277,17 +191,18 @@ enum JBSourceMode {
     [self addSubview:imageView];
     
     // Generates the animation
-    [UIView beginAnimations:nil context:NULL];
-    [UIView setAnimationDuration:_showImageDuration + 2];
-    [UIView setAnimationCurve:UIViewAnimationCurveEaseIn];
-    CGAffineTransform rotate    = CGAffineTransformMakeRotation(rotation);
-    CGAffineTransform moveRight = CGAffineTransformMakeTranslation(moveX, moveY);
-    CGAffineTransform combo1    = CGAffineTransformConcat(rotate, moveRight);
-    CGAffineTransform zoomIn    = CGAffineTransformMakeScale(zoomInX, zoomInY);
-    CGAffineTransform transform = CGAffineTransformConcat(zoomIn, combo1);
-    imageView.transform = transform;
+    [UIView animateWithDuration:_showImageDuration + 2 delay:0.0 options:UIViewAnimationCurveEaseOut animations:^{
+        CGAffineTransform rotate    = CGAffineTransformMakeRotation(rotation);
+        CGAffineTransform move = CGAffineTransformTranslate(rotate, originX, originY);
+        CGAffineTransform zoomIn    = CGAffineTransformScale(move, scaleFactor, scaleFactor);
+        imageView.transform = zoomIn;
+    } completion:^(BOOL finished) {
+        
+    }];
+    
+    
     [UIView commitAnimations];
-
+    
     [self _notifyDelegate];
 
     if (_currentImageIndex == _imagesArray.count - 1) {
